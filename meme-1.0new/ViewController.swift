3c775d57f3ca24a28fdd6a7226ad866d7731cdbc
jacksparrow00 <mehndiratta.nitish038@gmail.com>
2016-10-29 01:13:13 +0530

    partial working version of meme-1.0

--- meme-1.0new/ViewController.swift
+++ meme-1.0new/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName : UIColor.black,
+            NSForegroundColorAttributeName : UIColor.white,
+            NSFontAttributeName : UIFont(name : "HelveticaNeue-CondensedBlack",size: 40)!,
+            NSStrokeWidthAttributeName : -3.0
+        ] as [String : Any]
+        
+        topTextfield.defaultTextAttributes = memeTextAttributes
+        bottomTextfield.defaultTextAttributes = memeTextAttributes
+        
+        self.topTextfield.delegate = self
+        self.bottomTextfield.delegate = self
+        
+        topTextfield.textAlignment = .center
+        bottomTextfield.textAlignment = .center
+        topTextfield.text = "TOP"
+        bottomTextfield.text = "BOTTOM"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var bottomTextfield: UITextField!
+    @IBOutlet weak var topTextfield: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func pickAnImageFromAlbum(_ sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate=self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
     }
-
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.image = image
+            dismiss(animated: true, completion: nil)
+        }
+        else{
+            let alertController = UIAlertController()
+            alertController.title = "Error"
+            alertController.message = "Wrong photo selected. Please ensure you are selecting the right photo"
+            
+            let alertAction = UIAlertAction(title: "OK", style: .default){ action in self.dismiss(animated: true, completion: nil)
+            }
+            alertController.addAction(alertAction)
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    @IBAction func pickAnImageFromCamera(_ sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM"{
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
 }
 
